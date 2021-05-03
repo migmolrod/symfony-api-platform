aca800271979dd58371c3f3bc5591312045af260
migmolrod <migmolrod@fastmail.es>
2021-05-04 01:09:19 +0200

    Created User service boilerplate

---
+++ api/src/Service/User/UserRegisterService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service\User;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRegisterService
+{
+    public function __construct()
+    {
+    }
+
+    public function create(Request $request): User
+    {
+    }
+}
