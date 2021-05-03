247103525637f3b7e83bf999d7edc10b2d7e2280
migmolrod <migmolrod@fastmail.es>
2021-05-04 00:07:54 +0200

    Added BaseRepository

---
+++ api/src/Repository/BaseRepository.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\Exception;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\Mapping\MappingException;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectRepository;
+
+abstract class BaseRepository
+{
+    private ManagerRegistry $managerRegistry;
+    protected Connection $connection;
+    protected ObjectRepository $objectRepository;
+
+    public function __construct(ManagerRegistry $managerRegistry, Connection $connection)
+    {
+        $this->managerRegistry = $managerRegistry;
+        $this->connection = $connection;
+        $this->objectRepository = $this->getEntityManager()->getRepository($this::entityClass());
+    }
+
+    abstract protected static function entityClass(): string;
+
+    /**
+     * @throws ORMException
+     */
+    public function persistEntity(object $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+    }
+
+    /**
+     * @throws MappingException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function flushData(): void
+    {
+        $this->getEntityManager()->flush();
+        $this->getEntityManager()->clear();
+    }
+
+    /**
+     * @param object $entity
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function saveEntity(object $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @param object $entity
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function removeEntity(object $entity): void
+    {
+        $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @throws Exception
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function executeFetchQuery(string $query, array $params = []): array
+    {
+        return $this->connection->executeQuery($query, $params)->fetchAllAssociative();
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function executeQuery(string $query, array $params = []): void
+    {
+        $this->connection->executeQuery($query, $params);
+    }
+
+    /**
+     * @return ObjectManager|EntityManager
+     */
+    private function getEntityManager(): ObjectManager
+    {
+        $entityManager = $this->managerRegistry->getManager();
+
+        if ($entityManager->isOpen()) {
+            return $entityManager;
+        }
+
+        return $this->managerRegistry->resetManager();
+    }
+}
