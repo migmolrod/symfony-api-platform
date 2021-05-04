f87b4992f97737fb8a5f783a4cd9670d6a2b5262
migmolrod <migmolrod@fastmail.es>
2021-05-05 00:59:07 +0200

    Added message queuing to register service

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -4,20 +4,26 @@ namespace App\Service\User;
 
 use App\Entity\User;
 use App\Exception\User\UserAlreadyExistsException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserRegisterService
 {
     private UserRepository $userRepository;
     private EncoderService $encoderService;
+    private MessageBusInterface $messageBus;
 
-    public function __construct(UserRepository $userRepository, EncoderService $encoderService)
+    public function __construct(UserRepository $userRepository, EncoderService $encoderService, MessageBusInterface $messageBus)
     {
         $this->userRepository = $userRepository;
         $this->encoderService = $encoderService;
+        $this->messageBus = $messageBus;
     }
 
     /**
@@ -37,6 +43,11 @@ class UserRegisterService
         } catch (\Exception $exception) {
             throw UserAlreadyExistsException::fromEmail($email);
         }
+
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
 
         return $user;
     }
