2921f459efcdc290d9551509726436767be0602b
migmolrod <migmolrod@fastmail.es>
2021-05-09 18:09:38 +0200

    Finished reset password operation in api service

--- api/src/Api/Action/User/ResetPassword.php
+++ api/src/Api/Action/User/ResetPassword.php
@@ -3,17 +3,29 @@
 namespace App\Api\Action\User;
 
 use App\Entity\User;
+use App\Service\User\ResetPasswordService;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use JsonException;
 use Symfony\Component\HttpFoundation\Request;
 
 class ResetPassword
 {
-    public function __construct()
+    private ResetPasswordService $resetPasswordService;
+
+    public function __construct(ResetPasswordService $resetPasswordService)
     {
 
+        $this->resetPasswordService = $resetPasswordService;
     }
 
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     * @throws JsonException
+     */
     public function __invoke(Request $request): User
     {
-
+        return $this->resetPasswordService->reset($request);
     }
 }
--- api/src/Service/User/ResetPasswordService.php
+++ api/src/Service/User/ResetPasswordService.php
@@ -5,6 +5,10 @@ namespace App\Service\User;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
+use App\Service\Request\RequestService;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use JsonException;
 use Symfony\Component\HttpFoundation\Request;
 
 class ResetPasswordService
@@ -19,8 +23,23 @@ class ResetPasswordService
         $this->encoderService = $encoderService;
     }
 
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     * @throws JsonException
+     */
     public function reset(Request $request): User
     {
+        $userId = RequestService::getField($request, 'userId');
+        $resetPasswordToken = RequestService::getField($request, 'resetPasswordToken');
+        $password = RequestService::getField($request, 'password');
 
+        $user = $this->userRepository->findOneByIdAndResetPasswordTokenOrFail($userId, $resetPasswordToken);
+        $user->setPassword($this->encoderService->generateEncodedPassword($user, $password));
+        $user->setResetPasswordToken(null);
+
+        $this->userRepository->save($user);
+
+        return $user;
     }
 }
