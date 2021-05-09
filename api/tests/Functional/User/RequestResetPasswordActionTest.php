06217e807034f4a9364a31f21f4c17b480ee6975
migmolrod <migmolrod@fastmail.es>
2021-05-09 22:31:14 +0200

    Created request reset password tests

---
+++ api/tests/Functional/User/RequestResetPasswordActionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Functional\User;
+
+use JsonException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use function json_encode;
+use function sprintf;
+
+class RequestResetPasswordActionTest extends UserTestBase
+{
+    /**
+     * @throws JsonException
+     */
+    public function testRequestResetPassword(): void
+    {
+        $payload = [
+            'email' => 'peter@api.com',
+        ];
+
+        self::$peter->request(
+            'POST',
+            sprintf('%s/request-reset-password', $this->endpoint),
+            [],
+            [],
+            [],
+            json_encode($payload, JSON_THROW_ON_ERROR)
+        );
+
+        $response = self::$peter->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
+    }
+    /**
+     * @throws JsonException
+     */
+    public function testRequestResetPasswordForNonExistingEmail(): void
+    {
+        $payload = [
+            'email' => 'non-existing@api.com',
+        ];
+
+        self::$peter->request(
+            'POST',
+            sprintf('%s/request-reset-password', $this->endpoint),
+            [],
+            [],
+            [],
+            json_encode($payload, JSON_THROW_ON_ERROR)
+        );
+
+        $response = self::$peter->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_NOT_FOUND, $response->getStatusCode());
+    }
+}
