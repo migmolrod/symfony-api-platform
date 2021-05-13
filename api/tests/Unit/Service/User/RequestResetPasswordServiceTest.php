843dd5d7d86dfd8f16b79ef93b68f289aeb26869
migmolrod <migmolrod@fastmail.es>
2021-05-13 22:30:26 +0200

    Fix previous commit

--- api/tests/Unit/Service/User/RequestResetPasswordServiceTest.php
+++ api/tests/Unit/Service/User/RequestResetPasswordServiceTest.php
@@ -2,7 +2,67 @@
 
 namespace App\Tests\Unit\Service\User;
 
-class RequestResetPasswordServiceTest
-{
+use App\Entity\User;
+use App\Exception\User\UserNotFoundException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Service\User\RequestResetPasswordService;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\Messenger\Envelope;
 
+class RequestResetPasswordServiceTest extends UserServiceTestBase
+{
+    private RequestResetPasswordService $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->service = new RequestResetPasswordService($this->userRepository, $this->messageBus);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function testRequestResetPassword(): void
+    {
+        $email = 'username@api.com';
+        $user = new User('username', $email);
+
+        $this->userRepository
+            ->expects(self::once())
+            ->method('findOneByEmailOrFail')
+            ->with($email)
+            ->willReturn($user);
+
+        $message = $this->getMockBuilder(UserRegisteredMessage::class)->disableOriginalConstructor()->getMock();
+        $this->messageBus
+            ->expects(self::once())
+            ->method('dispatch')
+            ->with(self::isType('object'), self::isType('array'))
+            ->willReturn(new Envelope($message));
+
+        $this->service->send($email);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function gestRequestResetPasswordForNonExistingUser(): void
+    {
+        $email = 'non-existing@api.com';
+        $user = new User('non-existing', $email);
+
+        $this->userRepository
+            ->expects(self::once())
+            ->method('findOneByEmailOrFail')
+            ->with($email)
+            ->willThrowException(new UserNotFoundException());
+
+        $this->expectException(UserNotFoundException::class);
+
+        $this->service->send($email);
+    }
 }
