3d79fc8f36ac959afa1c97e27e40f626aaa1e924
migmolrod <migmolrod@fastmail.es>
2021-06-03 23:26:10 +0200

    Added functional test to accept group requests

---
+++ api/tests/Functional/Group/AcceptRequestTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Tests\Functional\Group;
+
+use App\Exception\GroupRequest\GroupRequestNotFoundException;
+use Doctrine\DBAL\Driver\Exception as DoctrineDbalDriverException;
+use Doctrine\DBAL\Exception as DoctrineDbalException;
+use JsonException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AcceptRequestTest extends GroupTestBase
+{
+    /**
+     * @throws JsonException
+     * @throws DoctrineDbalException
+     * @throws DoctrineDbalDriverException
+     */
+    public function testAcceptRequest(): void
+    {
+        $brianId = $this->getBrianId();
+        $peterGroupId = $this->getPeterGroupId();
+        $payload = [
+            'userId' => $brianId,
+            'token' => '2345678',
+        ];
+
+        self::$client->request(
+            'PUT',
+            sprintf('%s/%s/accept-request', $this->endpoint, $peterGroupId),
+            [],
+            [],
+            [],
+            json_encode($payload, JSON_THROW_ON_ERROR),
+        );
+
+        $response = self::$client->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        self::assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
+        self::assertEquals('The user has been added to the group', $responseData['message']);
+    }
+
+    /**
+     * @throws JsonException
+     * @throws DoctrineDbalException
+     * @throws DoctrineDbalDriverException
+     */
+    public function testAcceptAnAlreadyRequest(): void
+    {
+        $this->testAcceptRequest();
+
+        $brianId = $this->getBrianId();
+        $peterGroupId = $this->getPeterGroupId();
+        $payload = [
+            'userId' => $brianId,
+            'token' => '2345678',
+        ];
+
+        self::$client->request(
+            'PUT',
+            sprintf('%s/%s/accept-request', $this->endpoint, $peterGroupId),
+            [],
+            [],
+            [],
+            json_encode($payload, JSON_THROW_ON_ERROR),
+        );
+
+        $response = self::$client->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        self::assertEquals(JsonResponse::HTTP_NOT_FOUND, $response->getStatusCode());
+        self::assertEquals(GroupRequestNotFoundException::class, $responseData['class']);
+    }
+}
