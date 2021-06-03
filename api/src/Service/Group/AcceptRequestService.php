ed6c4e9212410167a38b9e8a5dc38628bf0f799a
migmolrod <migmolrod@fastmail.es>
2021-06-03 22:39:07 +0200

    Added API Platform config to accept group request
    Also added security firewall exception for the endpoint

---
+++ api/src/Service/Group/AcceptRequestService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service\Group;
+
+class AcceptRequestService
+{
+    public function __construct()
+    {
+    }
+
+    public function accept(string $groupId, string $userId, string $token): void
+    {
+    }
+}
