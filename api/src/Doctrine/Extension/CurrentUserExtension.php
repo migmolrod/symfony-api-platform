ef8df27ef4919c7346b986bfdffa2bac8c22bbaa
migmolrod <migmolrod@fastmail.es>
2021-06-12 21:06:57 +0200

    Updated doctrine extension for categories
    Updated doctrine extension to securely retrive user and group category
    collections

--- api/src/Doctrine/Extension/CurrentUserExtension.php
+++ api/src/Doctrine/Extension/CurrentUserExtension.php
@@ -4,11 +4,15 @@ namespace App\Doctrine\Extension;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Category;
 use App\Entity\Group;
 use App\Entity\User;
+use App\Exception\Group\GroupNotFoundException;
+use App\Repository\GroupRepository;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use function sprintf;
 
 class CurrentUserExtension implements QueryCollectionExtensionInterface
 {
@@ -16,10 +20,12 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface
     private const USER_DENIED = 'You can\'t retrieve users of a group you don\'t belong to.';
 
     private TokenStorageInterface $tokenStorage;
+    private GroupRepository $groupRepository;
 
-    public function __construct(TokenStorageInterface $tokenStorage)
+    public function __construct(TokenStorageInterface $tokenStorage, GroupRepository $groupRepository)
     {
         $this->tokenStorage = $tokenStorage;
+        $this->groupRepository = $groupRepository;
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
@@ -29,23 +35,52 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface
 
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
+        $parameterId = $queryBuilder->getParameters()->first()->getValue();
+
         /** @var User|null $user */
         $user = $this->tokenStorage->getToken()
             ? $this->tokenStorage->getToken()->getUser()
             : null;
 
-        if ((Group::class === $resourceClass) && $queryBuilder->getParameters()->first()->getValue() !== $user->getId()) {
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        if ((Group::class === $resourceClass) && $parameterId !== $user->getId()) {
             throw new AccessDeniedHttpException(self::GROUP_DENIED);
         }
 
         if (User::class === $resourceClass) {
             foreach ($user->getGroups() as $group) {
-                if ($group->getId() === $queryBuilder->getParameters()->first()->getValue()) {
+                if ($group->getId() === $parameterId) {
                     return;
                 }
             }
 
             throw new AccessDeniedHttpException(self::USER_DENIED);
         }
+
+        if (Category::class === $resourceClass) {
+            if ($this->isGroupAndUserIsMember($parameterId, $user)) {
+                $queryBuilder->andWhere(sprintf('%s.group = :parameterId', $rootAlias));
+                $queryBuilder->setParameter('parameterId', $parameterId);
+            } else {
+                $queryBuilder->andWhere(sprintf('%s.%s = :user', $rootAlias, $this->getResources()[$resourceClass]));
+                $queryBuilder->andWhere(sprintf('%s.group IS NULL', $rootAlias));
+                $queryBuilder->setParameter('user', $user);
+            }
+        }
+    }
+
+    private function isGroupAndUserIsMember(string $parameterId, User $user): bool
+    {
+        try {
+            return $user->isMemberOfGroup($this->groupRepository->findOneByIdOrFail($parameterId));
+        } catch (GroupNotFoundException $exception) {
+            return false;
+        }
+    }
+
+    private function getResources(): array
+    {
+        return [Category::class => 'owner'];
     }
 }
