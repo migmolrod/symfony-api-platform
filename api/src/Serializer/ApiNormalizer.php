6d1cf355230f788fca98f115d2e72a91c109f3dc
migmolrod <migmolrod@fastmail.es>
2021-08-06 00:53:13 +0200

    Added decoration for responses
    Decorated avatar property in User entity responses, now
    they are full URIs that includes the Digital Ocean space
    domain so it's directly accessable.
    
    Also refactored custom date filter to be reusable.

---
+++ api/src/Serializer/ApiNormalizer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Serializer;
+
+use App\Entity\User;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class ApiNormalizer implements NormalizerInterface, DenormalizerInterface, SerializerAwareInterface
+{
+    private $decorated;
+    private string $mediaPath;
+
+    public function __construct(NormalizerInterface $decorated, string $mediaPath)
+    {
+        if (!$decorated instanceof DenormalizerInterface) {
+            throw new \InvalidArgumentException(sprintf('The decorated normalizer must implement the %s.', DenormalizerInterface::class));
+        }
+
+        $this->decorated = $decorated;
+        $this->mediaPath = $mediaPath;
+    }
+
+    public function supportsNormalization($data, $format = null): bool
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = $this->decorated->normalize($object, $format, $context);
+
+        // Generate full URI for avatar
+        if (($object instanceof User) && null !== $avatar = $object->getAvatar()) {
+            $data['avatar'] = $this->mediaPath.$avatar;
+        }
+
+        return $data;
+    }
+
+    public function supportsDenormalization($data, $type, $format = null): bool
+    {
+        return $this->decorated->supportsDenormalization($data, $type, $format);
+    }
+
+    public function denormalize($data, $type, $format = null, array $context = [])
+    {
+        return $this->decorated->denormalize($data, $type, $format, $context);
+    }
+
+    public function setSerializer(SerializerInterface $serializer): void
+    {
+        if ($this->decorated instanceof SerializerAwareInterface) {
+            $this->decorated->setSerializer($serializer);
+        }
+    }
+}
